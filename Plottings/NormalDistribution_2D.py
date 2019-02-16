8dc409c2babe1ee146cc950cab98d0aa1d5a8999
Chuan Yang <tritiums@outlook.com>
2019-02-16 17:26:49 +0800

    Normal Distribution 2D

--- Plottings/NormalDistribution_2D.py
+++ Plottings/NormalDistribution_2D.py
@@ -4,7 +4,15 @@ from mpl_toolkits import mplot3d
 import math
 
 def f(x, y):
-	z = x + y
+	rho = 0
+	a = 100
+	b = 100
+	sigma1 = 50
+	sigma2 = 50
+
+	i = -1/(2*(1-rho**2)) * ((x-a)**2/sigma1**2 - 2*rho*(x-a)*(y-b)/(sigma1*sigma2) + (y-b)**2/sigma2**2)
+
+	z = math.e**i / math.pi*2*sigma1*sigma2*math.sqrt(1-rho**2)
 	return z
 
 x = np.linspace(-50, 250, 30)
